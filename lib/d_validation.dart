5bea85ce35262a4a6eb7b2cf6101d99130f97f84
vtd182 <92201436+vtd182@users.noreply.github.com>
2025-01-12 17:19:41 +0700

    export widgets

--- lib/d_validation.dart
+++ lib/d_validation.dart
@@ -8,6 +8,8 @@ export 'src/localization/language_manager.dart';
 export 'src/localization/localization.dart';
 export 'src/types/types.dart';
 export 'src/validations/validations.dart';
+export 'src/widgets/d_validate_email_text_field.dart';
+export 'src/widgets/string_validator.dart';
 
 sealed class DValidation {
   static final globalConfig = _GlobalConfig(
